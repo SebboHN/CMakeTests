23afc7a9699755b1cf638c1ca279c52d077b57be
SebboHN <50019972+SebboHN@users.noreply.github.com>
2020-04-19 15:51:10 +0200

    First Commit

---
+++ CMakeTests/src/CMakeTests/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "CMakeTests/header.h"
+
+void hello() {
+
+	printf("Hallo");
+	return 0;
+}
---
+++ example/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "CMakeTests/header.h"
+
+int main() {
+
+
+	hello();
+	return 0;
+}
